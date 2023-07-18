a7de57adf809da5c9f1eba2cedcc9467a9991128
Angel D. Munoz <scythes.lol@gmail.com>
2023-07-18 11:03:50 -0600

    first commit

---
+++ src/Falco/Program.fs
@@ -0,0 +1,17 @@
+﻿open Falco
+open Falco.Routing
+open Falco.HostBuilder
+
+let handler context =
+    let route = Request.getRoute context
+
+    let name = // extract route values safely
+        match route.TryGetStringNonEmpty "Name" with
+        | Some name -> name
+        | None -> "F#"
+
+    // send back an http response
+    Response.ofPlainText $"Hello {name}" context
+
+// declare the host and the endpoints
+webHost [||] { endpoints [ get "/{Name?}" handler ] }
---
+++ src/Saturn/Program.fs
@@ -0,0 +1,18 @@
+﻿open Saturn
+open Giraffe
+
+let handler name next context =
+    let message = $"Hello %s{name}"
+    // return an http response
+    text message next context
+
+let appRouter =
+    router {
+        // bind route parameters safely
+        get "/" (handler "F#")
+        getf "/%s" handler
+    }
+
+let server = application { use_router appRouter }
+
+run server
