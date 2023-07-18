d0b099865fcefe215fbdfb23e4d9896bf9947e4f
Angel D. Munoz <angel.m@xuppay.com>
2023-07-18 15:07:12 -0600

    working sample

--- src/Falco/Program.fs
+++ src/Falco/Program.fs
@@ -1,17 +1,70 @@
-﻿open Falco
+﻿open System
+open System.IO
+open System.Threading.Tasks
+
+open Falco
 open Falco.Routing
 open Falco.HostBuilder
+open Falco.Markup
+open Microsoft.AspNetCore.Http
 
-let handler context =
-    let route = Request.getRoute context
+let responseTemplate color content =
 
-    let name = // extract route values safely
-        match route.TryGetStringNonEmpty "Name" with
-        | Some name -> name
-        | None -> "F#"
+    Elem.article
+        [ Attr.style $"color: %s{color}" ]
+        [ Elem.header [] [ Elem.h3 [] [ Text.enc "File Contents below:" ] ]
+          Elem.pre [] [ Text.enc content ] ]
 
-    // send back an http response
-    Response.ofPlainText $"Hello {name}" context
+let index (context: HttpContext) : Task =
+    task {
+        let! content = File.ReadAllTextAsync("../index.html", context.RequestAborted)
+        return! Response.ofHtmlString content context
+    }
+
+
+let handler context : Task =
+    task {
+        // Falco can also use aspnet's features directly
+        // but offers an F# API for ease of use
+        let! form = Request.streamForm context
+
+        let extractedFile =
+            // extract the file safely from the
+            // IFormFileCollection in the http context
+            form.Files
+            |> Option.bind (fun form ->
+                // try to extract the uploaded file named after the "name" attribute in html
+                // GetFile returns null if no file is present, so we safely convert it into an optional value
+                form.GetFile "my-uploaded-file" |> Option.ofObj)
+
+        match extractedFile with
+        | Some file ->
+            use reader = new StreamReader(file.OpenReadStream())
+
+            // read the file stream directly into text for simplicity
+            let! content = reader.ReadToEndAsync(context.RequestAborted)
+
+            // you could do this with the streams themselves to make it more performat
+            do! File.WriteAllTextAsync($"./{Guid.NewGuid()}.txt", content, context.RequestAborted)
+
+            // return a response for the request
+            let content = responseTemplate "green" content
+            return! Response.ofHtml content context
+        | None ->
+            // The file was not found in the request return something
+            let content = responseTemplate "tomato" "The file was not provided"
+
+            return! context |> Response.withStatusCode 400 |> Response.ofHtml content
+    }
+
 
 // declare the host and the endpoints
-webHost [||] { endpoints [ get "/{Name?}" handler ] }
+webHost [||] {
+    endpoints
+        [
+          // handle the index routes as well
+          get "/" index
+          get "/index.html" index
+          // our file endpoint handler as well
+          post "/uploads" handler ]
+}
--- src/Saturn/Program.fs
+++ src/Saturn/Program.fs
@@ -1,16 +1,53 @@
-﻿open Saturn
-open Giraffe
+﻿open System
+open System.IO
 
-let handler name next context =
-    let message = $"Hello %s{name}"
-    // return an http response
-    text message next context
+open Microsoft.AspNetCore.Http
+
+open Saturn
+open Giraffe
+open Giraffe.ViewEngine
+
+let responseTemplate color content =
+
+    article
+        [ _style $"color: %s{color}" ]
+        [ header [] [ h3 [] [ encodedText "File Contents below:" ] ]
+          pre [] [ encodedText content ] ]
+
+let index next context = htmlFile "../index.html" next context
+
+let handler next (context: HttpContext) =
+    task {
+        // Saturn/Giraffe can also use aspnet's features directly
+        let form = context.Request.Form
+        let extractedFile = form.Files.GetFile "my-uploaded-file" |> Option.ofObj
+
+        match extractedFile with
+        | Some file ->
+            use reader = new StreamReader(file.OpenReadStream())
+
+            // read the file stream directly into text for simplicity
+            let! content = reader.ReadToEndAsync()
+
+            // you could do this with the streams themselves to make it more performat
+            do! File.WriteAllTextAsync($"./{Guid.NewGuid()}.txt", content)
+
+            // return a response for the request
+            let content = responseTemplate "green" content
+            return! htmlView content next context
+        | None ->
+            // The file was not found in the request return something
+            let content = responseTemplate "tomato" "The file was not provided"
+
+            return! (setStatusCode 400 >> htmlView content) next context
+    }
 
 let appRouter =
     router {
         // bind route parameters safely
-        get "/" (handler "F#")
-        getf "/%s" handler
+        get "/" index
+        get "/index.html" index
+        post "/uploads" handler
     }
 
 let server = application { use_router appRouter }
